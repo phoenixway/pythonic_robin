38341854fbc75fd69de9102b1b8a981ad44cf579
Roman Kozak <cossack.roman@gmail.com>
2021-10-31 03:06:08 +0200

    refactor

--- bot.py
+++ bot.py
@@ -1,24 +1,20 @@
  #!/usr/bin/env python3
-from datetime import date
+from datetime import datetime
 
 def get_answer(msg, state):
-    status = 0
     answer = "Don't know what to say."
-    if msg == "quit":
-        status = 1
-
-    rule1 = {"type": "answer", "input": "helo", "output": "hey!"}
-    rule2 = {"type": "answer", "input": "what?", "output": "dont know"}
-    rule3 = {"type": "func", "input": "time", "func": lambda: (date.today())}
-    rule4 = {"type": "answer", "input": "quit", "output": "Have a nice day!"}
-    rule5 = {"type": "answer", "input": "", "output": "Welcome!"}
+    status = 1 if msg == "quit" else 0 
 
     rules = []
-    rules.append(rule1)
-    rules.append(rule2)
-    rules.append(rule3)
-    rules.append(rule4)
-    rules.append(rule5)
+    rules.append({
+        "type": "answer", 
+        "input": "helo", 
+        "output": "hey!"
+    })
+    rules.append({"type": "answer", "input": "what?", "output": "dont know"})
+    rules.append({"type": "func", "input": "time", "func": lambda: datetime.now().strftime("%d/%m/%Y %H:%M:%S")})
+    rules.append({"type": "answer", "input": "quit", "output": "Have a nice day!"})
+    rules.append({"type": "answer", "input": "", "output": "Welcome!"})
 
     for rule in rules:
         if rule["type"] == "answer" and rule["input"] == msg:
