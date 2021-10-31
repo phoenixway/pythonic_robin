04c1d3e2c80d79baf4a021079940f6bfaac97b53
Roman Kozak <cossack.roman@gmail.com>
2021-10-31 03:47:29 +0300

    rules

--- bot.py
+++ bot.py
@@ -1,21 +1,41 @@
  #!/usr/bin/env python3
+from datetime import date
 
 def get_answer(msg, state):
     status = 0
-    answer = ""
+    answer = "Don't know what to say."
     if msg == "quit":
         status = 1
-    script = {}
-    script[""] = "Welcome!"
-    script["quit"] = "Have a nice day!"
-    answer = script[msg] if msg in script else "Don't know what to say."
-    print(answer)
+
+    rule1 = {"type": "answer", "input": "helo", "output": "hey!"}
+    rule2 = {"type": "answer", "input": "what?", "output": "dont know"}
+    rule3 = {"type": "func", "input": "time", "func": lambda: (date.today())}
+    rule4 = {"type": "answer", "input": "quit", "output": "Have a nice day!"}
+    rule5 = {"type": "answer", "input": "", "output": "Welcome!"}
+
+    rules = []
+    rules.append(rule1)
+    rules.append(rule2)
+    rules.append(rule3)
+    rules.append(rule4)
+    rules.append(rule5)
+
+    for rule in rules:
+        if rule["type"] == "answer" and rule["input"] == msg:
+            answer = rule["output"]
+            break
+        elif rule["type"] == "func" and rule["input"] == msg:
+            answer = rule["func"]()
+            break
+
     return status, answer, state
 
 state = {}
 m = ""
 while True:
-    s, _, state = get_answer(m, state)
+    s, a, state = get_answer(m, state)
+    if a != "":
+        print(a)  
     if s == 1:
         break
     m = input(">>")
