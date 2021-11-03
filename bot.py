b731eeba694b6f029b54231edb39c8ab285395d6
Roman Kozak <cossack.roman@gmail.com>
2021-11-03 09:26:05 +0200

    rules from a file

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
  #!/usr/bin/env python3
 
-import re
-
 from pyleri.repeat import Repeat
 import grammar_parser
 from datetime import datetime
@@ -13,51 +11,31 @@ from pyleri import (
     List, 
     Sequence)
 
-class MyGrammar(Grammar):
-    #RE_KEYWORDS = re.compile(r'\S+')
-    RE_KEYWORDS = re.compile('^[A-Za-z-=>]+')
-    r_output = Regex('[a-zA-Z 0-9\']+')
-    r_input = Regex('[a-zA-Z 0-9\']+')
+f = open("script1.rules", "r")
+script = f.read()
 
-    # r_output = Regex('(?:"(?:[^"]*)")+')
-    # r_input = Regex('(?:"(?:[^"]*)")+')
-    k_lead2 = Token('=>')
-    START = Repeat(Sequence(r_input, k_lead2, r_output))
+my_grammar = grammar_parser.MyGrammar()
+res = my_grammar.parse(script)
+start = res.tree.children[0] if res.tree.children else res.tree
+rules = []
+if ( type(start.element) is Repeat) and (start.element.name == 'START'):
+    for item in start.children:
+        rule = grammar_parser.get_ast(item)
+        if rule != None: rules.append(rule)
 
-# Compile your grammar by creating an instance of the Grammar Class.
-my_grammar = MyGrammar()
-
-# Use the compiled grammar to parse 'strings'
-print(my_grammar.parse('text => Iris').is_valid) # => True
-print(my_grammar.parse('bye "Iris"').is_valid) # => False
-print(my_grammar.parse('text => Iris').as_str()) # => True
-print(my_grammar.parse('text => Iris')) # => True
-res = my_grammar.parse('text => Iris\ntext2 => TT2')
-start = res.tree
-#res.tree.children[0].element.name
-#res.tree.element.name
-#res.tree.element Repeat
-#res.tree.children[0].element Repeat
-#res.tree.children[0].children[0].element #Sequence
-#далі вже є ім'я і тип відповідає складовим statement`a
-print(grammar_parser.node_props(start, start.children))
-
-#print(my_grammar.parse('bye "Iris"').as_str()) # => error at position 0, expecting: hi
+rules.append({
+    "type": "answer", 
+    "input": "helo", 
+    "output": "hey!"
+})
+rules.append({"type": "answer", "input": "what?", "output": "dont know"})
+rules.append({"type": "func", "input": "time", "func": lambda: datetime.now().strftime("%d/%m/%Y %H:%M:%S")})
+rules.append({"type": "answer", "input": "quit", "output": "Have a nice day!"})
+rules.append({"type": "answer", "input": "", "output": "Welcome!"})
 
 def get_answer(msg, state):
     answer = "Don't know what to say."
     status = 1 if msg == "quit" else 0 
-
-    rules = []
-    rules.append({
-        "type": "answer", 
-        "input": "helo", 
-        "output": "hey!"
-    })
-    rules.append({"type": "answer", "input": "what?", "output": "dont know"})
-    rules.append({"type": "func", "input": "time", "func": lambda: datetime.now().strftime("%d/%m/%Y %H:%M:%S")})
-    rules.append({"type": "answer", "input": "quit", "output": "Have a nice day!"})
-    rules.append({"type": "answer", "input": "", "output": "Welcome!"})
 
     for rule in rules:
         if rule["type"] == "answer" and rule["input"] == msg:
--- grammar_parser.py
+++ grammar_parser.py
@@ -1,5 +1,23 @@
  #!/usr/bin/env python3
 
+import re
+
+from pyleri import (
+    Grammar,
+    Keyword,
+    Token,
+    Regex,
+    List, 
+    Sequence)
+from pyleri.repeat import Repeat
+
+class MyGrammar(Grammar):
+    RE_KEYWORDS = re.compile('^[A-Za-z-=>]+')
+    r_output = Regex('[a-zA-Z 0-9\']+')
+    r_input = Regex('[a-zA-Z 0-9\']+')
+    k_lead2 = Token('=>')
+    START = Repeat(Sequence(r_input, k_lead2, r_output))
+
 def node_props(node, children):
     return {
         'start': node.start,
@@ -11,3 +29,13 @@ def node_props(node, children):
 
 def get_children(children):
     return [node_props(c, get_children(c.children)) for c in children]
+
+def get_ast(node):
+    if (type(node.element) is Sequence) and (len(node.children) == 3):
+        return {
+            'type': 'answer',
+            'input': node.children[0].string,
+            'output': node.children[2].string
+        }
+    else:
+        return None
