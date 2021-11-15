65d20b230bf596879f0eb644d79873d2a2aa6f40
Roman Kozak <cossack.roman@gmail.com>
2021-11-15 10:11:05 +0200

    refactoring

--- bot.py
+++ bot.py
@@ -1,37 +1,7 @@
  #!/usr/bin/env python3
+from rules_engine import RulesEngine
 
-from pyleri.repeat import Repeat
-import grammar_parser
-from datetime import datetime
-from pyleri import (
-    Grammar,
-    Keyword,
-    Token,
-    Regex,
-    List, 
-    Sequence)
-
-f = open("script1.rules", "r")
-script = f.read()
-
-my_grammar = grammar_parser.MyGrammar()
-res = my_grammar.parse(script)
-start = res.tree.children[0] if res.tree.children else res.tree
-rules = []
-if ( type(start.element) is Repeat) and (start.element.name == 'START'):
-    for item in start.children:
-        rule = grammar_parser.get_ast(item)
-        if rule != None: rules.append(rule)
-
-rules.append({
-    "type": "answer", 
-    "input": "helo", 
-    "output": "hey!"
-})
-rules.append({"type": "answer", "input": "what?", "output": "dont know"})
-rules.append({"type": "func", "input": "time", "func": lambda: datetime.now().strftime("%d/%m/%Y %H:%M:%S")})
-rules.append({"type": "answer", "input": "quit", "output": "Have a nice day!"})
-rules.append({"type": "answer", "input": "", "output": "Welcome!"})
+rules = RulesEngine().rules
 
 def get_answer(msg, state):
     answer = "Don't know what to say."
