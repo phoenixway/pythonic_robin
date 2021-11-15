39e0af803a68a94a43944b3c75b49335c21ce2ce
Roman Kozak <cossack.roman@gmail.com>
2021-11-15 14:18:45 +0200

    minor

--- tests/test_rules_engine.py
+++ tests/test_rules_engine.py
@@ -4,10 +4,14 @@ import unittest
 import ai_engine
 
 class TestRules(unittest.TestCase):
+    def setUp(self) -> None:
+        self.ai = ai_engine.AI()
+        self.ai.rules_engine.loadFromFile("script1.rules")
+        self.ai.isTesting = True
+        return super().setUp()
+
     def test_1(self):
-        ai = ai_engine.AI()
-        ai.rules_engine.loadFromFile("script1.rules")
-        ai.isTesting = True
-        status, answer, state = ai.get_answer("hello", {})
+        
+        status, answer, state = self.ai.get_answer("hello", {})
         self.assertIsNot(status, 1)
         self.assertTrue(answer == "hey!")
