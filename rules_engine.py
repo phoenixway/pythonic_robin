833c24809655a95720386561055d767bd983d3d7
Roman Kozak <cossack.roman@gmail.com>
2021-11-15 11:39:39 +0200

    refactoring

---
+++ rules_engine.py
@@ -0,0 +1,20 @@
+ #!/usr/bin/env python3
+
+import rs_parser
+from pyleri.repeat import Repeat
+
+class RulesEngine():
+    def __init__(self) -> None:
+        self.grammar = rs_parser.MyGrammar()
+    
+    def loadFromFile(self, name):
+        f = open(name, "r")
+        script = f.read()
+        res = self.grammar.parse(script)
+        start = res.tree.children[0] if res.tree.children else res.tree
+        self.rules = []
+        if ( type(start.element) is Repeat) and (start.element.name == 'START'):
+            for item in start.children:
+                rule = rs_parser.get_ast(item)
+                if rule != None: self.rules.append(rule)
+        return self.rules
