cf055bdb6f4396b03e826a8600ea29c3dbb2e4f7
Roman Kozak <cossack.roman@gmail.com>
2021-11-15 11:41:48 +0200

    refactoring

--- ai_engine.py
+++ ai_engine.py
@@ -1,31 +1,31 @@
  #!/usr/bin/env python3
 
-from rules_runner import RulesEngine
+from rules_engine import RulesEngine
 from datetime import datetime
 
 rules_engine = RulesEngine()
-rules = rules_engine.loadFromFile("script1.rules")
+rules_engine.loadFromFile("script1.rules")
 
-rules.append({
+rules_engine.rules.append({
     "type": "answer", 
     "input": "hello", 
     "output": "hey!"
 })
-rules.append({
+rules_engine.rules.append({
     "type": "func", 
     "input": "time", 
     "func": lambda: datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 })
-rules.append({"type": "answer", "input": "quit", "output": "Have a nice day!"})
-rules.append({"type": "answer", "input": "", "output": "Welcome!"})
-rules.append({"type": "answer", "input": "what?", "output": "dont know"})
+rules_engine.rules.append({"type": "answer", "input": "quit", "output": "Have a nice day!"})
+rules_engine.rules.append({"type": "answer", "input": "", "output": "Welcome!"})
+rules_engine.rules.append({"type": "answer", "input": "what?", "output": "dont know"})
 
 
 def get_answer(msg, state):
     answer = "Don't know what to say."
     status = 1 if msg == "quit" else 0 
 
-    for rule in rules:
+    for rule in rules_engine.rules:
         if rule["type"] == "answer" and rule["input"] == msg:
             answer = rule["output"]
             break
