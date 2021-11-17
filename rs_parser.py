218976de5582748db731de6a0a24aa05d6893299
Roman Kozak <cossack.roman@gmail.com>
2021-11-17 16:14:02 +0200

    cleaning

--- rs_parser.py
+++ rs_parser.py
@@ -4,25 +4,9 @@ import re
 from pyparsing import *
 import pyparsing as pp
 
-from pyleri import (
-    Grammar,
-    Keyword,
-    Token,
-    Regex,
-    List, 
-    Sequence)
-from pyleri.repeat import Repeat
-
-class RobinScriptGrammar(Grammar):
-    RE_KEYWORDS = re.compile('^[A-Za-z =>]+')
-    r_output = Regex('[a-zA-Z 0-9\']+')
-    r_input = Regex('[a-zA-Z 0-9\']+')
-    k_lead2 = Token('>>')
-    START = Repeat(Sequence(r_input, k_lead2, r_output))
-
+class RobinScriptGrammar():
     statement = Forward()
     #suite = indentedBlock(statement)
-
     text = alphanums + ".,!? "
     input = Word(text)('input')
     output = Word(text)('output')
@@ -32,36 +16,10 @@ class RobinScriptGrammar(Grammar):
     statement.setResultsName("statement")
     module_body = OneOrMore(statement)
 
-
-def node_props(node, children):
-    return {
-        'start': node.start,
-        'end': node.end,
-        'name': node.element.name if hasattr(node.element, 'name') else None,
-        'element': node.element.__class__.__name__,
-        'string': node.string,
-        'children': children}
-
-def get_children(children):
-    return [node_props(c, get_children(c.children)) for c in children]
-
-def get_ast(node):
-    if (type(node.element) is Sequence) and (len(node.children) == 3):
-        return {
-            'type': 'answer',
-            'input': node.children[0].string,
-            'output': node.children[2].string
-        }
-    else:
-        return None
-
 def getRules(pyparserResults):
     rules = []
     for item in pyparserResults:
-            #print("Is {} a in2out? {}".format(item, 'in2out' in item))
             if 'in2out' in item:
-                #print("Content is: {}".format(item['in2out']))
-                #print("Input: {}, output: {}".format(item['in2out'][0], item['in2out'][1]))
                 input = item['in2out'][0]
                 output = item['in2out'][1]
                 rules.append({
--- rules_engine.py
+++ rules_engine.py
@@ -1,7 +1,6 @@
  #!/usr/bin/env python3
 
 import rs_parser
-from pyleri.repeat import Repeat
 
 class RulesEngine():
     def __init__(self) -> None:
@@ -11,12 +10,5 @@ class RulesEngine():
     def loadFromFile(self, name):
         f = open(name, "r")
         script = f.read()
-        # res = self.grammar.parse(script)
-        # start = res.tree.children[0] if res.tree.children else res.tree
-        # if ( type(start.element) is Repeat) and (start.element.name == 'START'):
-        #     for item in start.children:
-        #         rule = rs_parser.get_ast(item)
-        #         if rule != None: self.rules.append(rule)
-        # print('lala')
         self.rules.extend(rs_parser.getRules(self.grammar.module_body.parseString(script)))
 
