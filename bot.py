d74b1c5e021ee2e158fcb089b6ceb4545591eb4e
Roman Kozak <cossack.roman@gmail.com>
2021-11-18 14:52:48 +0200

    minor

--- bot.py
+++ bot.py
@@ -1,5 +1,23 @@
  #!/usr/bin/env python3
 import ai_engine
+import os
+import readline
+import atexit
+
+histfile = os.path.join(os.path.expanduser("~"), ".rs_history")
+h_len = 0
+try:
+    readline.read_history_file(histfile)
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    open(histfile, 'wb').close()
+
+def save(prev_h_len, histfile):
+    new_h_len = readline.get_current_history_length()
+    readline.set_history_length(1000)
+    readline.append_history_file(new_h_len - prev_h_len, histfile)
+    
+atexit.register(save, h_len, histfile)
 
 state = {}
 state['status'] = 'default'
